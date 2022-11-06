06ac7bd3701540384803a7baa651d45241f115a7
Muratbek Omurzakov <82247467+omur7ak0v@users.noreply.github.com>
2022-11-06 22:51:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function menu() {
+  document.getElementById("burger").classList.toggle("menu_active");
+}
+
+// modal images
+
+function gallery_1() {
+  document.getElementById("gallery_1").classList.toggle("gallery_1");
+}
+function gallery_2() {
+  document.getElementById("gallery_2").classList.toggle("gallery_2");
+}
+function gallery_3() {
+  document.getElementById("gallery_3").classList.toggle("gallery_3");
+}
+function gallery_4() {
+  document.getElementById("gallery_4").classList.toggle("gallery_4");
+}
+function gallery_5() {
+  document.getElementById("gallery_5").classList.toggle("gallery_5");
+}
+function gallery_6() {
+  document.getElementById("gallery_6").classList.toggle("gallery_6");
+}
